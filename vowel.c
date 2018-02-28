91003b0cc0c61eaad109eea4909804dcc01445a8
indhuvsb <36696610+indhuvsb@users.noreply.github.com>
2018-02-28 02:19:36 -0800

    cpg
    good

---
+++ vowel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int lowervalue,uppervalue;
+printf("enter the lowervalues of A");
+scanf("%d"\n);
+else
+printf("constant value of C");
+return 0;
+}
